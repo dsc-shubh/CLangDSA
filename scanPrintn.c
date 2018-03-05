adb235cc1d1b876ed804646fe8147adc8d2a8c64
Prashant Kumar <prashant153@gmail.com>
2018-03-05 19:06:48 +0530

    Delete scanPrintn.c

--- scanPrintn.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-	int n;
-	printf("Please enter the value of n. \n" );
-	scanf("%d",&n);
-	printf("The value of n = %d \n",&n );
-	return 0;
-}
