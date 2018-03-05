fe15c64bc951362f2b755564d3e7040ce45e8ced
Prashant Kumar <prashant153@gmail.com>
2018-03-05 18:56:50 +0530

    Added basic programs

---
+++ scanPrintn.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n;
+	printf("Please enter the value of n. \n" );
+	scanf("%d",&n);
+	printf("The value of n = %d \n",&n );
+	return 0;
+}
