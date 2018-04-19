032c92d8ff2c2e9263c0e6e98f65bf3384395634
prashant153 <prashant153@gmail.com>
2018-04-19 12:42:05 +0530

    Hello world program using Xcode

---
+++ HelloWorldXcode/HelloWorldXcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorldXcode
+//
+//  Created by Prashant Kumar on 4/19/18.
+//  Copyright © 2018 Prashant Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
