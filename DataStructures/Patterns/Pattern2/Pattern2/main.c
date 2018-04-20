94b60932d1f9a3601255a96c13186ac03bc79daa
prashant153 <prashant153@gmail.com>
2018-04-20 09:27:26 +0530

    Added Pattern 2 triangle

---
+++ DataStructures/Patterns/Pattern2/Pattern2/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  Pattern2
+//
+//  Created by Prashant Kumar on 4/20/18.
+//  Copyright © 2018 Prashant Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for (int i = 1; i<=5; i++) {
+        for (int j = 1; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//*
+//* *
+//* * *
+//* * * *
+//* * * * * 
