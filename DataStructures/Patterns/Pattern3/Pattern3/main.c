343223fde620ed95c2ef28f9c11010c655bc4c12
prashant153 <prashant153@gmail.com>
2018-04-20 09:34:41 +0530

    Added Pattern 3 triangle

---
+++ DataStructures/Patterns/Pattern3/Pattern3/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  Pattern3
+//
+//  Created by Prashant Kumar on 4/20/18.
+//  Copyright © 2018 Prashant Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for (int i = 1; i<=5; i++) {
+        for (int j = i; j<=5; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
